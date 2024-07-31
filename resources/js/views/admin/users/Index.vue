c1459a19ace44c7d555ec137cf7859a42eb9e15e
Alexey <l.e.x.a_64@mail.ru>
2024-07-31 17:32:25 +0300

    Users page: filters, table head

--- resources/js/views/admin/users/Index.vue
+++ resources/js/views/admin/users/Index.vue
@@ -3,14 +3,14 @@
         <div class="col-md-12">
             <div class="card border-0">
                 <div class="card-header bg-transparent">
-                    <h5 class="float-start">Users</h5>
+                    <h5 class="float-start">{{ $t('users.users')}}</h5>
                     <router-link v-if="can('user-list')" :to="{ name: 'users.create' }" class="btn btn-primary btn-sm float-end">
                         Add New
                     </router-link>
                 </div>
                 <div class="card-body shadow-sm">
                     <div class="mb-4">
-                        <input v-model="search_global" type="text" placeholder="Search..."
+                        <input v-model="search_global" type="text" :placeholder="$t('users.search_global')"
                                class="form-control w-25">
                     </div>
                     <div class="table-responsive">
@@ -20,95 +20,100 @@
                                 <th class="px-6 py-3 bg-gray-50 text-left">
                                     <input v-model="search_id" type="text"
                                            class="inline-block mt-1 form-control"
-                                           placeholder="Filter by ID">
+                                           :placeholder="$t('users.filter_by_id')">
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left">
                                     <input v-model="search_title" type="text"
                                            class="inline-block mt-1 form-control"
-                                           placeholder="Filter by Title">
+                                           :placeholder="$t('users.filter_by_name')">
                                 </th>
+                                <th class="px-6 py-3 text-start"></th>
                                 <th class="px-6 py-3 text-start"></th>
                                 <th class="px-6 py-3 text-start"></th>
                             </tr>
                             <tr>
-                                <th class="px-6 py-3 text-start">
-                                    <div class="flex flex-row"
-                                         @click="updateOrdering('id')">
+                                <th class="px-6 py-3 text-start" style="cursor: pointer;"
+                                    @click="updateOrdering('id')">
+                                    <div class="flex flex-row">
                                         <div class="font-medium text-uppercase"
                                              :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
-                                            ID
+                                            {{ $t('users.id') }}
+                                            <i class="bi bi-chevron-expand"></i>
                                         </div>
-                                        <div class="select-none">
-                                <span :class="{
-                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'id',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'id',
-                                }">&uarr;</span>
+                                        <!--<div class="select-none">
                                             <span :class="{
-                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'id',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'id',
-                                }">&darr;</span>
-                                        </div>
+                                              'text-blue-600': orderDirection === 'asc' && orderColumn === 'id',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'id',
+                                            }">&uarr;</span>
+                                            <span :class="{
+                                              'text-blue-600': orderDirection === 'desc' && orderColumn === 'id',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'id',
+                                            }">&darr;</span>
+                                        </div>-->
                                     </div>
                                 </th>
-                                <th class="px-6 py-3 text-left">
-                                    <div class="flex flex-row"
-                                         @click="updateOrdering('title')">
-                                        <div class="font-medium text-uppercase"
+                                <th class="px-6 py-3 text-left" style="cursor: pointer;"
+                                    @click="updateOrdering('title')">
+                                    <div class="flex flex-row">
+                                        <div class="font-medium"
                                              :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
-                                            Name
+                                            {{ $t('users.name') }}
+                                            <i class="bi bi-chevron-expand"></i>
                                         </div>
-                                        <div class="select-none">
-                                <span :class="{
-                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'title',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'title',
-                                }">&uarr;</span>
+                                        <!--<div class="select-none">
                                             <span :class="{
-                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'title',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'title',
-                                }">&darr;</span>
-                                        </div>
+                                                'text-blue-600': orderDirection === 'asc' && orderColumn === 'title',
+                                                'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'title',
+                                            }">&uarr;</span>
+                                            <span :class="{
+                                                'text-blue-600': orderDirection === 'desc' && orderColumn === 'title',
+                                                'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'title',
+                                            }">&darr;</span>
+                                        </div>-->
                                     </div>
                                 </th>
-                                <th class="px-6 py-3 text-left">
-                                    <div class="flex flex-row"
-                                         @click="updateOrdering('email')">
-                                        <div class="font-medium text-uppercase"
+                                <th class="px-6 py-3 text-left" style="cursor: pointer;"
+                                    @click="updateOrdering('email')">
+                                    <div class="flex flex-row">
+                                        <div class="font-medium"
                                              :class="{ 'font-bold text-blue-600': orderColumn === 'email' }">
-                                            Email
+                                            {{ $t('users.email') }}
+                                            <i class="bi bi-chevron-expand"></i>
                                         </div>
-                                        <div class="select-none">
-                                <span :class="{
-                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'email',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'email',
-                                }">&uarr;</span>
+                                        <!--<div class="select-none">
                                             <span :class="{
-                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'email',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'email',
-                                }">&darr;</span>
-                                        </div>
+                                              'text-blue-600': orderDirection === 'asc' && orderColumn === 'email',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'email',
+                                            }">&uarr;</span>
+                                            <span :class="{
+                                              'text-blue-600': orderDirection === 'desc' && orderColumn === 'email',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'email',
+                                            }">&darr;</span>
+                                        </div>-->
                                     </div>
                                 </th>
-                                <th class="px-6 py-3 bg-gray-50 text-left">
-                                    <div class="flex flex-row items-center justify-between cursor-pointer"
-                                         @click="updateOrdering('created_at')">
+                                <th class="px-6 py-3 bg-gray-50 text-left" style="cursor: pointer;"
+                                    @click="updateOrdering('created_at')">
+                                    <div class="flex flex-row">
                                         <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
                                              :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
-                                            Created at
+                                            {{ $t('users.created_at') }}
+                                            <i class="bi bi-chevron-expand"></i>
                                         </div>
-                                        <div class="select-none">
-                                <span :class="{
-                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'created_at',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'created_at',
-                                }">&uarr;</span>
+                                        <!--<div class="select-none">
                                             <span :class="{
-                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'created_at',
-                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'created_at',
-                                }">&darr;</span>
-                                        </div>
+                                              'text-blue-600': orderDirection === 'asc' && orderColumn === 'created_at',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'created_at',
+                                            }">&uarr;</span>
+                                            <span :class="{
+                                              'text-blue-600': orderDirection === 'desc' && orderColumn === 'created_at',
+                                              'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'created_at',
+                                            }">&darr;</span>
+                                        </div>-->
                                     </div>
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left">
-                                    Actions
+                                    {{ $t('users.actions') }}
                                 </th>
                             </tr>
                             </thead>
