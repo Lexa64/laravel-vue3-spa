b16817a5ee143b4f24fcccbfd77720424d0b71fd
Alexey <l.e.x.a_64@mail.ru>
2024-09-12 17:21:19 +0300

    Costs temp

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -95,7 +95,7 @@ class ProjectController extends Controller
 
         $data = [];
 
-        $data['resource_consumption']['1_1_2'] = round($project->square_living, 2);
+        /*$data['resource_consumption']['1_1_2'] = round($project->square_living, 2);
         $data['economically_justified']['1_1_2'] = round($project->square_living * $project->tariff->ut_1_1_2_economically_justified * 12, 2);
         $data['state_subsidized']['1_1_2'] = round($project->square_living * $project->tariff->ut_1_1_2_state_subsidized * 12, 2);
 
@@ -149,19 +149,89 @@ class ProjectController extends Controller
 
         $data['resource_consumption']['9'] = $project->square_living;
         $data['economically_justified']['9'] = round($project->square_living * $project->tariff->ut_9_economically_justified * 12, 2);
-        $data['state_subsidized']['9'] = round($project->square_living * $project->tariff->ut_9_state_subsidized * 12, 2);
+        $data['state_subsidized']['9'] = round($project->square_living * $project->tariff->ut_9_state_subsidized * 12, 2);*/
 
-        $this->calculateOneMeter($project->utilityCost->toArray(), $project->square_living);
+        $data['resource_consumption']['1_1'] = round($project->utilityCost->uc_1_1, 2);
+        $data['economically_justified']['1_1'] = round($project->utilityCost->uc_1_1 * $project->tariff->ut_1_1_economically_justified, 2);
+        $data['state_subsidized']['1_1'] = round($project->utilityCost->uc_1_1 * $project->tariff->ut_1_1_state_subsidized, 2);
+
+        $data['resource_consumption']['1_2'] = $project->utilityCost->uc_1_2;
+        $data['economically_justified']['1_2'] = round($project->utilityCost->uc_1_2 * $project->tariff->ut_1_2_economically_justified, 2);
+        $data['state_subsidized']['1_2'] = round($project->utilityCost->uc_1_2 * $project->tariff->ut_1_2_state_subsidized, 2);
+
+        $data['resource_consumption']['1_3'] = $project->utilityCost->uc_1_3;
+        $data['economically_justified']['1_3'] = round($project->utilityCost->uc_1_3 * $project->tariff->ut_1_3_economically_justified, 2);
+        $data['state_subsidized']['1_3'] = round($project->utilityCost->uc_1_3 * $project->tariff->ut_1_3_state_subsidized, 2);
+
+        $data['resource_consumption']['1_4'] = $project->utilityCost->uc_1_4;
+        $data['economically_justified']['1_4'] = round($project->utilityCost->uc_1_4 * $project->tariff->ut_1_4_economically_justified, 2);
+        $data['state_subsidized']['1_4'] = round($project->utilityCost->uc_1_4 * $project->tariff->ut_1_4_state_subsidized, 2);
+
+        $data['resource_consumption']['2'] = $project->utilityCost->uc_2;
+        $data['economically_justified']['2'] = round($project->utilityCost->uc_2 * $project->tariff->ut_2_economically_justified, 2);
+        $data['state_subsidized']['2'] = round($project->utilityCost->uc_2 * $project->tariff->ut_2_state_subsidized, 2);
+
+        $data['resource_consumption']['3'] = $project->utilityCost->uc_3;
+        $data['economically_justified']['3'] = round($project->utilityCost->uc_3 * $project->tariff->ut_3_economically_justified, 2);
+        $data['state_subsidized']['3'] = round($project->utilityCost->uc_3 * $project->tariff->ut_3_state_subsidized, 2);
+
+        $data['resource_consumption']['3_1'] = $project->utilityCost->uc_3_1;
+        $data['economically_justified']['3_1'] = round($project->utilityCost->uc_3_1 * $project->tariff->ut_3_1_economically_justified, 2);
+        $data['state_subsidized']['3_1'] = round($project->utilityCost->uc_3_1 * $project->tariff->ut_3_1_state_subsidized, 2);
+
+        $data['resource_consumption']['3_2'] = $project->utilityCost->uc_3_2;
+        $data['economically_justified']['3_2'] = round($project->utilityCost->uc_3_2 * $project->tariff->ut_3_2_economically_justified, 2);
+        $data['state_subsidized']['3_2'] = round($project->utilityCost->uc_3_2 * $project->tariff->ut_3_2_state_subsidized, 2);
+
+        /*$data['resource_consumption']['4_1'] = $project->utilityCost->uc_4_1;
+        $data['economically_justified']['4_1'] = round($project->utilityCost->uc_4_1 * $project->tariff->ut_4_1_economically_justified, 2);
+        $data['state_subsidized']['4_1'] = round($project->utilityCost->uc_4_1 * $project->tariff->ut_4_1_state_subsidized, 2);
+
+        $data['resource_consumption']['4_2'] = $project->utilityCost->uc_4_2;
+        $data['economically_justified']['4_2'] = round($project->utilityCost->uc_4_2 * $project->tariff->ut_4_2_economically_justified, 2);
+        $data['state_subsidized']['4_2'] = round($project->utilityCost->uc_4_2 * $project->tariff->ut_4_2_state_subsidized, 2);
+
+        $data['resource_consumption']['5'] = $project->utilityCost->uc_5;
+        $data['economically_justified']['5'] = round($project->utilityCost->uc_5 * $project->tariff->ut_5_economically_justified, 2);
+        $data['state_subsidized']['5'] = round($project->utilityCost->uc_5 * $project->tariff->ut_5_state_subsidized, 2);
+
+        $data['resource_consumption']['6_1'] = $project->square_living;
+        $data['economically_justified']['6_1'] = round($project->square_living * $project->tariff->ut_6_1_economically_justified * 12, 2);
+        $data['state_subsidized']['6_1'] = round($project->square_living * $project->tariff->ut_6_1_state_subsidized * 12, 2);
+
+        $data['resource_consumption']['6_3'] = $project->square_living;
+        $data['economically_justified']['6_3'] = round($project->square_living * $project->tariff->ut_6_3_economically_justified * 12, 2);
+        $data['state_subsidized']['6_3'] = round($data['economically_justified']['6_3'] * $project->tariff->ut_6_3_state_subsidized * 12, 2);
+
+        $data['resource_consumption']['7_2'] = 2.68 * $project->residents_count;
+        $data['economically_justified']['7_2'] = round(2.68 * $project->residents_count * $project->tariff->ut_7_2_economically_justified, 2);
+        $data['state_subsidized']['7_2'] = round(2.68 * $project->residents_count * $project->tariff->ut_7_2_state_subsidized, 2);
+
+        $data['resource_consumption']['8_2'] = $project->square_living;
+        $data['economically_justified']['8_2'] = round($project->square_living * $project->tariff->ut_8_2_economically_justified * 12, 2);
+        $data['state_subsidized']['8_2'] = round($project->square_living * $project->tariff->ut_8_2_state_subsidized * 12, 2);
+
+        $data['resource_consumption']['9'] = $project->square_living;
+        $data['economically_justified']['9'] = round($project->square_living * $project->tariff->ut_9_economically_justified * 12, 2);
+        $data['state_subsidized']['9'] = round($project->square_living * $project->tariff->ut_9_state_subsidized * 12, 2);*/
+
+        $temp = 0;
+
+        //$this->calculateOneMeter($project->utilityCost->toArray(), $project->square_living);
 
         return $data;
     }
 
-    public function calculateOneMeter($utilityCost, $square_living)
+    public function calculateOneMeter($utilityCost, $square_living): array
     {
+        $oneMeterData = [];
+
         foreach ($utilityCost as $key => $value) {
             if (str_starts_with($key, 'uc_')) {
                 $oneMeterData[$key] = round($value / $square_living, 2);
             }
         }
+
+        return $oneMeterData;
     }
 }
